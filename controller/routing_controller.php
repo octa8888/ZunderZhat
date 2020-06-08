5d077ab287a2494651f51c598db0f42f4f0a6219
octa8888 <malvin00.com@gmail.com>
2020-06-09 01:16:53 +0700

    init

---
+++ controller/routing_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class RoutingController{
+    public function index(){
+        include('index.php');
+    }
+
+    public function test(){
+        include('test.php');
+    }
+}
---
+++ router.php
@@ -0,0 +1,22 @@
+<?php
+
+$url_arr=explode('/',$_SERVER['REQUEST_URI']);
+
+include('controller/routing_controller.php');
+
+$controller=new RoutingController();
+
+$url=$url_arr[sizeof($url_arr)-1];
+
+if($url==''){
+    $controller->index();
+}
+else{
+    if(method_exists($controller, $url)){
+        $controller->$url();
+    }
+    else{
+        // echo "Not Exists";
+        $controller->index();
+    }
+}
